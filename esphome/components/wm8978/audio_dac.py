95a314420ada76802b2b88ee7f33893054171e86
Hendrik L <hendrik+dev@xd0.de>
2026-01-19 12:31:02 +0100

    float_range

--- esphome/components/wm8978/audio_dac.py
+++ esphome/components/wm8978/audio_dac.py
@@ -10,14 +10,12 @@ DEPENDENCIES = ["i2c"]
 wm8978_ns = cg.esphome_ns.namespace("wm8978")
 WM8978 = wm8978_ns.class_("WM8978", AudioDac, cg.Component, i2c.I2CDevice)
 
-WM8978_MIC_GAINS = [-12, -11.25, -10.5, -9.75, -9, -8.25, -7.5, -6.75, -6, -5.25, -4.5, -3.75, -3, -2.25, -1.5, -0.75, 0, 0.75, 1.5, 2.25, 3, 3.75, 4.5, 5.25, 6, 6,75, 7.5, 8.25, 9, 9.75, 10.5, 11.25, 12, 12,75, 13.5, 14.25, 15, 15.75, 16.5, 17.25, 18, 18.75, 19.5, 20.25, 21, 21.75, 22.5, 23.25, 24, 24.75, 25.5, 26.25, 27, 27.75, 28.5, 29.25, 30, 30.75, 31.5, 32.25, 33, 33.75, 34.5, 35.25]
-
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(WM8978),
             cv.Optional(CONF_MIC_GAIN, default="0db"): cv.All(
-                cv.decibel, cv.one_of(*WM8978_MIC_GAINS)
+                cv.decibel, cv.float_range(min=-12.0, max=35.25)
             ),
         }
     )
